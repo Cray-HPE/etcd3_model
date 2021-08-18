3f37cb575511f2ee53f5a3ede143c1bed5fb3df1
Eric Lund <eric.lund@hpe.com>
2021-08-18 10:27:39 -0500

    Add GitHub build related files

---
+++ etcd3_model/print_version.py
@@ -0,0 +1,35 @@
+"""Print Version Information for Compute Rolling Upgrade
+
+MIT License
+
+(C) Copyright [2020] Hewlett Packard Enterprise Development LP
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+# This code is here to support builds.  It is not incorporated into
+# the python module in any meaningful way.
+
+# pragma should never happen
+from version import VERSION
+
+# pragma should never happen
+print(VERSION)
+# pragma should never happen
+exit(0)
